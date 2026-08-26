5aa587e222cbbf1e2ab66abf72e2c92bf1e69acc
SirGhaniR <ghanifaustaraihan@gmail.com>
2026-08-26 11:44:03 +0700

    feat: dark and light theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,56 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: TodoScreen());
+    return MaterialApp(
+      theme: ThemeData.light().copyWith(
+        primaryColor: Colors.blue,
+        colorScheme: ColorScheme.light(
+          primary: Colors.blue,
+          secondary: Colors.blueAccent,
+        ),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+        ),
+      ),
+      darkTheme: ThemeData.dark().copyWith(
+        primaryColor: Colors.indigo,
+        colorScheme: ColorScheme.dark(
+          primary: Colors.indigo,
+          secondary: Colors.indigoAccent,
+        ),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.indigo[800],
+          foregroundColor: Colors.white,
+        ),
+        scaffoldBackgroundColor: Colors.grey[900],
+        cardColor: Colors.grey[850],
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey[700]!),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey[700]!),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.indigoAccent),
+          ),
+        ),
+      ),
+      themeMode: ThemeMode.system, // Follows system preference by default
+      home: TodoScreen(),
+    );
   }
 }
 
 class TodoScreen extends StatefulWidget {
-  const new({super.key});
+  const TodoScreen({super.key});
 
   @override
   TodoScreenState createState() => TodoScreenState();
@@ -50,7 +86,10 @@ class TodoScreenState extends State<TodoScreen> {
           content: TextField(
             controller: editController,
             autofocus: true,
-            decoration: InputDecoration(hintText: 'Enter new task name...'),
+            decoration: InputDecoration(
+              hintText: 'Enter new task name...',
+              border: OutlineInputBorder(),
+            ),
           ),
           actions: [
             TextButton(
@@ -127,6 +166,8 @@ class TodoScreenState extends State<TodoScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Todo List For The Great SirGhani'),
@@ -154,7 +195,10 @@ class TodoScreenState extends State<TodoScreen> {
                   ),
                 ),
                 SizedBox(width: 8),
-                ElevatedButton(onPressed: addTodo, child: Text('Add')),
+                ElevatedButton(
+                  onPressed: addTodo,
+                  child: Text('Add'),
+                ),
               ],
             ),
           ),
@@ -162,30 +206,39 @@ class TodoScreenState extends State<TodoScreen> {
             child: ListView.builder(
               itemCount: todos.length,
               itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(
-                    todos[index],
-                    style: TextStyle(
-                      decoration: isDone[index]
-                          ? TextDecoration.lineThrough
-                          : TextDecoration.none,
-                      color: isDone[index] ? Colors.grey : Colors.black,
+                return Card(
+                  margin: EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+                  elevation: isDark ? 0 : 1,
+                  color: isDark ? Colors.grey[850] : null,
+                  child: ListTile(
+                    title: Text(
+                      todos[index],
+                      style: TextStyle(
+                        decoration: isDone[index]
+                            ? TextDecoration.lineThrough
+                            : TextDecoration.none,
+                        color: isDone[index]
+                            ? (isDark ? Colors.grey[600] : Colors.grey)
+                            : (isDark ? Colors.white : Colors.black),
+                      ),
                     ),
-                  ),
-                  leading: IconButton(
-                    icon: Icon(
-                      isDone[index]
-                          ? Icons.check_circle
-                          : Icons.radio_button_unchecked,
-                      color: isDone[index] ? Colors.green : Colors.grey,
+                    leading: IconButton(
+                      icon: Icon(
+                        isDone[index]
+                            ? Icons.check_circle
+                            : Icons.radio_button_unchecked,
+                        color: isDone[index]
+                            ? Colors.green
+                            : (isDark ? Colors.grey[500] : Colors.grey),
+                      ),
+                      onPressed: () => toggleDone(index),
                     ),
-                    onPressed: () => toggleDone(index),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete, color: Colors.red),
+                      onPressed: () => deleteTodo(index),
+                    ),
+                    onLongPress: () => editTask(index),
                   ),
-                  trailing: IconButton(
-                    icon: Icon(Icons.delete, color: Colors.red),
-                    onPressed: () => deleteTodo(index),
-                  ),
-                  onLongPress: () => editTask(index),
                 );
               },
             ),
@@ -193,20 +246,23 @@ class TodoScreenState extends State<TodoScreen> {
           SafeArea(
             child: Container(
               padding: EdgeInsets.all(16),
-              color: Colors.grey[200],
+              color: isDark ? Colors.grey[850] : Colors.grey[200],
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text(
                     'Total: ${todos.length} tasks',
-                    style: TextStyle(fontSize: 16),
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: isDark ? Colors.white70 : null,
+                    ),
                   ),
                   Text(
                     'Left: ${todos.where((done) => !isDone[todos.indexOf(done)]).length}',
                     style: TextStyle(
                       fontSize: 16,
                       fontWeight: FontWeight.bold,
-                      color: Colors.blue,
+                      color: isDark ? Colors.indigoAccent : Colors.blue,
                     ),
                   ),
                 ],
